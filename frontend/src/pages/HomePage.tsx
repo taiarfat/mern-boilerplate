f644fd6e9b25bc458923dd1519b0800efaf5962f
Tai Arfat <tai.arfat@bacancy.com>
2025-04-05 14:00:56 +0530

    refactor: simplify HomePage component by removing AppBar and Toolbar

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,32 +3,19 @@ import {
   Container, 
   Box, 
   Typography, 
-  Button, 
-  AppBar, 
-  Toolbar 
 } from '@mui/material';
 
 const HomePage: React.FC = () => {
 
 
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Dashboard
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      
-      <Container component="main">
+    <Container component="main">
         <Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <Typography component="h1" variant="h4" gutterBottom>
             Welcome to the Bac Hack
           </Typography>
         </Box>
       </Container>
-    </>
   );
 };
 
